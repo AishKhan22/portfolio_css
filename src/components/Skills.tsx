336d1ef81d1b5176802b69ba679c889c5bed07a7
AishKhan22 <aish.khan712@gmail.com>
2024-11-10 21:03:34 +0500

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
     <div> 
         <h2 className='text-4xl md:text-5xl font-bold'> Technologies I work with </h2>
         <p className='text-[#605678] pt-2'>
-        Welcome to my portfolio! I'm a dedicated developer with a strong foundation in TypeScript, Java, HTML, CSS, and Next.js. My work focuses on creating robust and responsive web applications that emphasize user experience and clean design. Here, you'll find projects showcasing my ability to blend creativity with technical expertise, building interactive and efficient solutions tailored to meet modern web standards.
+        Welcome to my portfolio! I am a dedicated developer with a strong foundation in TypeScript, Java, HTML, CSS, and Next.js. My work focuses on creating robust and responsive web applications that emphasize user experience and clean design. Here, you will find projects showcasing my ability to blend creativity with technical expertise, building interactive and efficient solutions tailored to meet modern web standards.
         </p>
     </div>
     <div> 
